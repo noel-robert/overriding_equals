ec8cf747a3544ff3c25a4f05148b9835aeda752b
shahid <shahdbest@gmail.com>
2023-03-14 17:10:22 +0530

    problem

---
+++ src/main/java/com/stackroute/oops/Address.java
@@ -0,0 +1,26 @@
+package com.stackroute.oops;
+
+public class Address {
+
+    public Address() {
+        //default constructor
+    }
+
+    public Address(String area, String city) {
+    }
+
+    public String getArea() {
+        return null;
+    }
+
+    public void setArea(String area) {
+    }
+
+    public String getCity() {
+        return null;
+    }
+
+    public void setCity(String city) {
+    }
+
+}
---
+++ src/main/java/com/stackroute/oops/Customer.java
@@ -0,0 +1,39 @@
+package com.stackroute.oops;
+
+public class Customer {
+
+    public Customer() {
+        //default Constructor
+    }
+
+    public Customer(int customerId, String name, String email, Address address) {
+    }
+
+    public int getCustomerId() {
+        return -1;
+    }
+
+    public void setCustomerId(int customerId) {
+    }
+
+    public String getName() {
+        return null;
+    }
+
+    public void setName(String name) {
+    }
+
+    public String getEmail() {
+        return null;
+    }
+
+    public void setEmail(String email) {
+    }
+
+    public Address getAddress() {
+        return null;
+    }
+
+    public void setAddress(Address address) {
+    }
+}
