c8d38a0ac594d0fe9e99b80e40c8b3f56e9ff0d8
Noel John Robert <noel060202@gmail.com>
2024-05-10 22:53:58 +0530

    completed implementation, and passed all test cases

--- src/main/java/com/stackroute/oops/Address.java
+++ src/main/java/com/stackroute/oops/Address.java
@@ -1,26 +1,57 @@
 package com.stackroute.oops;
 
 public class Address {
+    private String area;
+    private String city;
 
     public Address() {
         //default constructor
     }
 
     public Address(String area, String city) {
+        this.area = area;
+        this.city = city;
     }
 
     public String getArea() {
-        return null;
+        return area;
     }
 
     public void setArea(String area) {
+        this.area = area;
     }
 
     public String getCity() {
-        return null;
+        return city;
     }
 
     public void setCity(String city) {
+        this.city = city;
     }
+
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Address other = (Address) obj;
+        if (area == null) {
+            if (other.area != null)
+                return false;
+        } else if (!area.equals(other.area))
+            return false;
+        if (city == null) {
+            if (other.city != null)
+                return false;
+        } else if (!city.equals(other.city))
+            return false;
+        return true;
+    }
+
+    
 
 }
--- src/main/java/com/stackroute/oops/Customer.java
+++ src/main/java/com/stackroute/oops/Customer.java
@@ -2,38 +2,82 @@ package com.stackroute.oops;
 
 public class Customer {
 
+    private int customerId;
+    private String name;
+    private String email;
+    private Address address;
+    
     public Customer() {
-        //default Constructor
     }
 
     public Customer(int customerId, String name, String email, Address address) {
+        this.customerId = customerId;
+        this.name = name;
+        this.email = email;
+        this.address = address;
     }
 
     public int getCustomerId() {
-        return -1;
+        return customerId;
     }
 
     public void setCustomerId(int customerId) {
+        this.customerId = customerId;
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public String getEmail() {
-        return null;
+        return email;
     }
 
     public void setEmail(String email) {
+        this.email = email;
     }
 
     public Address getAddress() {
-        return null;
+        return address;
     }
 
     public void setAddress(Address address) {
+        this.address = address;
     }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Customer other = (Customer) obj;
+        if (customerId != other.customerId)
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (email == null) {
+            if (other.email != null)
+                return false;
+        } else if (!email.equals(other.email))
+            return false;
+        if (address == null) {
+            if (other.address != null)
+                return false;
+        } else if (!address.equals(other.address))
+            return false;
+        return true;
+    }
+
+    
 }
